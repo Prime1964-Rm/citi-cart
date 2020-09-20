b0afdcf4df76b30fb2e4954c8c86506d6bf38dea
Rishabh Mishra <rishabh.mishra1564@gmail.com>
2020-09-20 16:14:26 +0530

    Updates

---
+++ precache-manifest.f51c6eb0a82b1ac7d86980c00558506c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25efd62732fc416172ceeb4a15c2b3e9",
+    "url": "/citi-cart/index.html"
+  },
+  {
+    "revision": "48f2854a263d5566bb43",
+    "url": "/citi-cart/static/css/main.d3c646fb.chunk.css"
+  },
+  {
+    "revision": "b95f680cbe8f9644b3dc",
+    "url": "/citi-cart/static/js/2.1afe3fdb.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/citi-cart/static/js/2.1afe3fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f2854a263d5566bb43",
+    "url": "/citi-cart/static/js/main.5c0ba7cf.chunk.js"
+  },
+  {
+    "revision": "1f0fce99d9237b4cde3a",
+    "url": "/citi-cart/static/js/runtime-main.ea179720.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/citi-cart/static/media/citilogo.ed28accb.svg"
+  },
+  {
+    "revision": "53a94e533b2f0a3f6b152b1e5fe437a4",
+    "url": "/citi-cart/static/media/seo-and-web.53a94e53.svg"
+  }
+]);
