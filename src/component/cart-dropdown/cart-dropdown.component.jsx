7ad7fed898303d9083e7e5fa67b0ae9d583cf307
Rishabh Mishra <rishabh.mishra1564@gmail.com>
2020-10-02 21:04:46 +0530

    Added redux bindings and Cart feature

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import {connect} from 'react-redux';
+import CartItem from '../carti-tem-component/cartItemComponent';
 import CustomButton from '../Custom-Button/CustomButton'
 import './cart-dropdown.styles.css'
 
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => {
+    return(
     <div className='cart-dropdown'>
-        <div className='cart-items' /> 
+        <div className='cart-items'>
+        {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+        </div> 
     <CustomButton>GO TO CHECKOUT</CustomButton>
         
     </div>
-)
-export default CartDropDown
+)}
+
+const mapStateToProps = (state) => {
+    return{
+        cartItems: state.cart.cartItems
+    }
+}
+export default connect(mapStateToProps,null)(CartDropDown)
